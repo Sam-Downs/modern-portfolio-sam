cc8f8ab6b2ec132b1fd8106c8f6341766e42189e
Sam-Downs <Sam_Downs@insideranken.org>
2023-03-16 08:01:59 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,6 @@ import Experience from "@/components/Experience"
 import Skills from "@/components/Skills"
 import Projects from "@/components/Projects"
 import Contact from "@/components/Contact"
-import { Canvas } from 'react-three-fiber';
 
 const inter = Inter({ subsets: ['latin'] })
 
