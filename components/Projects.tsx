df6b629cdba3a73fa47452865d80baf89b4ad77e
Sam-Downs <Sam_Downs@insideranken.org>
2023-04-05 21:17:10 -0500

    reformatted for issues

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -91,17 +91,15 @@ export default function App() {
         )}
         {loaded && instanceRef.current && (
           <div className='dots'>
-            {[...Array(instanceRef.current.track.details.slides.length).keys()].map((idx) => {
-              return (
-                <button
-                  key={idx}
-                  onClick={() => {
-                    instanceRef.current?.moveToIdx(idx);
-                  }}
-                  className={'dot' + (currentSlide === idx ? ' active' : '')}
-                ></button>
-              );
-            })}
+            {Array.from({ length: instanceRef.current.track.details.slides.length }, (_, idx) => (
+              <button
+                key={idx}
+                onClick={() => {
+                  instanceRef.current?.moveToIdx(idx);
+                }}
+                className={'dot' + (currentSlide === idx ? ' active' : '')}
+              ></button>
+            ))}
           </div>
         )}
       </div>
